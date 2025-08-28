34254184138f4f244621d9dfa731937762a5c126
Chaimae.bk <sha663277@gmail.com>
2025-08-28 13:00:43 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#calculator
+numr = float(input('enter first number : \n'))
+num = float(input('enter second number:\n'))
+op = input('choose operation : + , - , /, *, // \n')
+
+if op == '+':
+	print(numr+num)
+elif op == '-':
+	print(numr - num)
+elif op == '*':
+	print(numr * num)
+elif op == '//':
+	print(numr//num)
+else :
+	print(numr/num)
+
+
+	
+
+
+
+
